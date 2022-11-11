ea71a0bed7022dcd8bf440d441e9ea42a7261fd9
HP <irredel3@gmail.com>
2022-11-11 08:46:12 +0200

    first commit

---
+++ targil.js
@@ -0,0 +1,10 @@
+function mTrailer() {
+    x = document.getElementById("1").innerHTML="In October 1988, twelve-year-old Bill Denbrough crafts a paper sailboat for Georgie, his six-year-old brother. Georgie sails the boat along the rainy streets of small town Derry, Maine, only to have it fall down a storm drain. As he attempts to retrieve it, Georgie sees a clown in the drain, who introduces himself as Pennywise the Dancing Clown. Pennywise entices Georgie to come closer, then rips his arm off and drags him into the sewer."
+    y = document.getElementById("1").innerHTML="Kevin Wendell Crumb, a man struggling with dissociative identity disorder (DID) rooted in his history of childhood abuse and abandonment, has been managing living with his 23 distinct personalities well for several years with help from his therapist, Dr. Karen Fletcher"
+    if ("1" == x){
+        document.getElementById('1').innerHTML=x
+    }
+    else{
+        document.getElementById('1').innerHTML=y
+    }
+}
